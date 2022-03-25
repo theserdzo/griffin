2279e8630a51706e388cec480b591947db79e8de
Sergio <serdzo@yahoo.com>
2022-03-25 12:14:49 +0500

    contact-mobile-fix

--- js/tooplate-script.js
+++ js/tooplate-script.js
@@ -10,7 +10,7 @@ function resizeParallaxes() {
     const parallax2 = $('#parallax-2');
     const newHeight1 = calculateHeight(parallax1, 1800, 690);
     const newHeight2 = calculateHeight(parallax2, 1800, 590);
-    parallax1.css('min-height', newHeight1 + 'px');
+    // parallax1.css('min-height', newHeight1 + 'px');
     parallax2.css('min-height', newHeight2 + 'px');
 }
 
